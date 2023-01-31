9af79c34ba206671fa2338f5789ffda026e92cfb
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-01-31 16:32:56 +0100

    Starting code

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
 import pandas as pd
 import datetime as dt
-from random import randint, choice
+import random
 import smtplib
+from random import choice, randint
 
-MY_EMAIL = "EMAIL@GMAIL.COM"
-MY_PASSWORD = "123456789DEZ"
+MY_EMAIL = "example@XXXXX.com"  # your email
+MY_PASSWORD = "XXXXXXXXXXX"  # app password of your account
 
+# ---------------------------- DISCOVERING WHO IS CELEBRATING BIRTHDAY TODAY ------------------------------- #
+
+# Reading csv file, creating string month + day
+data = pd.read_csv("birthdays.csv")
+data["MONTH_DAY"] = data['month'].astype(str) + data['day'].astype(str)
+
+# Date now, creating string month + day
 now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1:
-    pd.read_excel(".xlsx")
+day = now.day
+month = now.month
+check_date = str(month) + str(day)
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs=MY_PASSWORD,
-                            msg="Subject: XXXX\n\n"
-                                "Message")
+# Checking number of people celebrating birthday today
+found = (check_date in data['MONTH_DAY'].unique())
+information_birthday = data.loc[data["MONTH_DAY"] == check_date]
+number_people = information_birthday["email"].count()
 
+for i in range(0, number_people):
+    # If true, retrieve information
+    if found:
+        information = data.loc[data["MONTH_DAY"] == check_date]
+        information_name = information["name"].iloc[i]
+        information_email = information["email"].iloc[i]
 
+        # ---------------------------- SELECTING RANDOM LETTER ------------------------------- #
 
+        letters_list = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+        random_letter = random.choice(letters_list)
+
+        # Editing selected letter with correct information
+        with open(str(random_letter)) as letter:
+            letter_edited = letter
+            letter_edited = letter_edited.read()
+            letter_edited = letter_edited.replace('[NAME]', f'{information_name}')
+            letter.close()
+            print(letter_edited)
+
+        # ---------------------------- SENDING EMAIL ------------------------------- #
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=information_email,
+                                msg=f"Subject: Happy birthday {information_name}\n\n"
+                                    f"{letter_edited}")
+    else:
+        pass
